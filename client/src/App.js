ea534fbcba1204085c26487f332d38c35fda2a8c
BAEK <100k1@naver.com>
2022-04-26 12:15:44 +0900

    라우터 기본 상태까지 설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,11 @@
 import React from "react";
-import { BrowserRouter as WebRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./page/Home";
-import Login from "./page/Login";
-import Register from "./page/Register";
+import RootRouter from "./routers/RootRouter";
 
 function App() {
   return (
-    <WebRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </WebRouter>
+    <>
+      <RootRouter />
+    </>
   );
 }
 
