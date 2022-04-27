81c98d74b2ee4ac3e5c0af17c174a83d0c9255f5
BAEK <100k1@naver.com>
2022-04-27 22:11:47 +0900

    스닙핏 포스팅 까지 함

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,10 +5,20 @@ import Axios from "axios";
 //홈 콤포넌트
 function Home() {
   const [snippets, setSnippets] = useState([]); //빈 배열을 안해놔서 생긴 오류 발견 !
+  const [newSnippetEditorOpen, setNewSnippetEditorOpen] = useState(false); // Form 태그 열게 할까 말까
+  const [editorTitle, setEditorTitle] = useState(""); // 인풋박스에 적은 벨류값 저장
+  const [editorDescription, setEditorDescription] = useState("");
+  const [editorCode, setEditorCode] = useState("");
 
   useEffect(() => {
     getAllSnippets();
   }, []);
+
+  useEffect(() => {
+    console.log("제목:" + editorTitle);
+    console.log("내용:" + editorDescription);
+    console.log("코드:" + editorCode);
+  }, [editorTitle, editorDescription, editorCode]);
 
   async function getAllSnippets() {
     const snippetsRes = await Axios.get("http://localhost:3000/snippet");
@@ -24,10 +34,61 @@ function Home() {
     });
   }
 
+  async function saveSnippet(e) {
+    e.preventDefault();
+
+    const snippetData = {
+      title: editorTitle ? editorTitle : undefined,
+      description: editorDescription ? editorDescription : undefined,
+      code: editorCode ? editorCode : undefined,
+    };
+
+    await Axios.post("http://localhost:3000/snippet/", snippetData);
+  }
+
   return (
     <div className="home">
       <p>홈페이지</p>
-      <div className="snippetsContainer">{spreadSnippets()}</div>
+      <div className="snippetsContainer">
+        {!newSnippetEditorOpen && (
+          <button onClick={() => setNewSnippetEditorOpen(true)}>
+            스니핏 추가
+          </button>
+        )}
+        {newSnippetEditorOpen && (
+          <div className="snippet-editor">
+            <form onSubmit={saveSnippet}>
+              <label htmlFor="editor-title">Title</label>
+              <input
+                id="editor-title"
+                type="text"
+                value={editorTitle}
+                onChange={(e) => setEditorTitle(e.target.value)}
+              />
+
+              <label htmlFor="editor-description">Description</label>
+              <input
+                id="editor-description"
+                type="text"
+                value={editorDescription}
+                onChange={(e) => setEditorDescription(e.target.value)}
+              />
+
+              <label htmlFor="editor-code">Code</label>
+              <textarea
+                id="editor-code"
+                type="text"
+                value={editorCode}
+                onChange={(e) => setEditorCode(e.target.value)}
+              />
+
+              <button type="submit">Save snippet</button>
+            </form>
+            <hr />
+          </div>
+        )}
+        {spreadSnippets()}
+      </div>
     </div>
   );
 }
